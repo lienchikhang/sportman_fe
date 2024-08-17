bd15ea2456d2e734f8790e6299363dbc7413fde8
Khang Lien <lienchikhang777@gmail.com>
2024-08-17 16:02:46 +0700

    [client][mess:: adjust footer]

--- client/src/libs/constants/footer.ts
+++ client/src/libs/constants/footer.ts
@@ -1,64 +1,37 @@
 const footer = [
     {
-        heading: "Showrooms",
+        heading: "Shop",
         child: [
-            "25 Ba Trieu Street, Hoan Kiem District, Hanoi, Vietnam",
-            "135 Nguyen Hue Boulevard, District 1, Ho Chi Minh City, Vietnam",
-            "88 Tran Phu Street, Nha Trang, Khanh Hoa Province, Vietnam",
-            "12 Le Loi Street, Hue City, Thua Thien Hue Province, Vietnam",
-            "10 Tran Phu Street, Da Nang City, Vietnam"
+            "homepage",
+            "shopper Community"
         ],
     },
     {
-        heading: "Help",
+        heading: "About",
         child: [
-            "Order Status",
-            "Delivery",
-            "Returns",
-            "Payment Options",
-            "Contact Us",
-        ],
-    },
-    {
-        heading: "About SportMan",
-        child: [
-            "About SportMan",
-            "News",
+            "Our mission",
             "Careers",
-            "Investors",
-            "Sustainability",
+            "Parnership",
+            "FAQ",
+            "Article",
         ],
     },
     {
-        heading: "Community",
+        heading: "Support",
         child: [
-            "Community Hub",
-            "Customer Success Stories",
-            "Forum",
-            "Events",
-            "Blog",
-            "Podcast",
-            "Creators",
-            "Invite a Friend",
-            "Become a Seller",
-            "Community Standards",
-            "        Affiliates",
+            "Contact",
+            "ORDER & RETURNS",
+            "HELP CENTER",
         ],
     },
     {
-        heading: "Business Solutions",
+        heading: "SELL",
         child: [
-            "About Business Solutions",
-            "Fiverr Pro",
-            "Fiverr Certified",
-            "Become an Agency",
-            "Fiverr Enterprise",
-            "ClearVoice",
-            "Content Marketing",
-            "Working Not Working",
-            "Contact Sales",
+            "SELL ON SPORTMAN",
+            "SELLER FACEBOOK GROUP"
         ],
     },
+
 ];
 
 
